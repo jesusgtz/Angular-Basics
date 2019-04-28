40a80981ac743e7ecc60550d98f227a51fcb2f4c
Jesus Gutierrez Sandoval <jesus.gutierrez@springlabs.net>
2019-04-28 15:45:38 -0500

    Creacion basica de componentes

---
+++ src/app/videojuego/videojuego.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'videojuegos',
+	template: `
+	<h2>Componente videojuego</h2>
+	`
+})
+export class VideojuegosComponent{
+	nombre = 'Videojuegos';
+}
