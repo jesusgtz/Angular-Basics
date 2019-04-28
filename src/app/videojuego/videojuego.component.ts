8676c8da9695cb5fa6901c01e869d2f10e1a59f0
Jesus Gutierrez Sandoval <jesus.gutierrez@springlabs.net>
2019-04-28 16:12:32 -0500

    Implementacion de directiva condicional

--- src/app/videojuego/videojuego.component.ts
+++ src/app/videojuego/videojuego.component.ts
@@ -4,8 +4,13 @@ import { Component } from '@angular/core';
 	selector: 'videojuegos',
 	template: `
 	<h2>Componente videojuego</h2>
+	<h3 *ngIf="mostrar_retro != true">El mejor juego es: {{ mejor_juego }}</h3>
+	<h3 [style.background]="'red'" *ngIf="mostrar_retro">El mejor juego es: {{ mejor_juego_retro }}</h3>
 	`
 })
 export class VideojuegosComponent{
 	nombre = 'Videojuegos';
+	mejor_juego = 'FIFA';
+	mejor_juego_retro = 'SuperMario 64';
+	mostrar_retro = true;
 }
